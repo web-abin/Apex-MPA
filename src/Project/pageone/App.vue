0cba0cee221252963aff187a3e82379f97df6fb9
王俊彬 <1872495742@qq.com>
2024-02-26 16:37:44 +0800

    test

--- src/Project/pageone/App.vue
+++ src/Project/pageone/App.vue
@@ -1,8 +1,8 @@
-<template>子页面1</template>
+<template>{{ test }}</template>
 
 <script setup>
 const test = ref('Composition API 自动引入')
-console.log(test.value)
+  console.log(  test.value)
 </script>
 <style lang="scss">
 #app {
